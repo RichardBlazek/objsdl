dd8fc133ce9413908f3741522f571ca969def81c
risa2 <blazek.risa@gmail.com>
2017-09-17 20:01:09 +0200

    font.h-shorter methods

--- decl/font.h
+++ decl/font.h
@@ -3,18 +3,18 @@
 class Font: public NonCopyable
 {
 private:
-    TTF_Font* font=nullptr;
-    void ErrorIfNotOpened()
-    {
+	TTF_Font* font=nullptr;
+	void ErrorIfNotOpened()
+	{
 		if(!font)
 		{
 			throw Error("Font is not opened");
 		}
-    }
+	}
 public:
 	enum class Style
 	{
-        Normal=TTF_STYLE_NORMAL,
+		Normal=TTF_STYLE_NORMAL,
 		Bold=TTF_STYLE_BOLD,
 		Italic=TTF_STYLE_ITALIC,
 		Underline=TTF_STYLE_UNDERLINE,
@@ -29,7 +29,7 @@ public:
 	};
 	void Close()
 	{
-        if(font)
+		if(font)
 		{
 			TTF_CloseFont(font);
 			font=nullptr;
@@ -38,14 +38,14 @@ public:
 	void Open(const std::string& filename, uint32 size)
 	{
 		Close();
-        font=TTF_OpenFont(filename.c_str(), size);
-        Error::IfZero(font);
+		font=TTF_OpenFont(filename.c_str(), size);
+		Error::IfZero(font);
 	}
 	void Open(const std::string& filename, uint32 size, uint32 index)
 	{
-        Close();
-        font=TTF_OpenFontIndex(filename.c_str(), size, index);
-        Error::IfZero(font);
+		Close();
+		font=TTF_OpenFontIndex(filename.c_str(), size, index);
+		Error::IfZero(font);
 	}
 	bool IsOpened()const
 	{
@@ -54,89 +54,71 @@ public:
 	Font()=default;
 	Font(const std::string& filename, uint32 size)
 	{
-        Open(filename, size);
+		Open(filename, size);
 	}
 	Font(const std::string& filename, uint32 size, uint32 index)
 	{
-        Open(filename, size, index);
+		Open(filename, size, index);
 	}
 	Font(Font&& init):font(init.font)
 	{
-        init.font=nullptr;
+		init.font=nullptr;
 	}
 	Font& operator=(Font&& init)
 	{
-        font=init.font;
-        init.font=nullptr;
-        return *this;
+		font=init.font;
+		init.font=nullptr;
+		return *this;
 	}
 	Surface Render(const std::string& text, Color textcolor)
 	{
 		ErrorIfNotOpened();
-		Surface result(TTF_RenderUTF8_Blended(font, text.empty()?" ":text.c_str(), Surface::ColorSDL(textcolor)));
-        Error::IfZero(result.surface);
-        return result;
+		return (Surface&&)Surface(Error::IfZero(TTF_RenderUTF8_Blended(font, text.empty()?" ":text.c_str(), Surface::ColorSDL(textcolor))));
 	}
 	Surface Render(const std::u16string& text, Color textcolor)
 	{
 		ErrorIfNotOpened();
-		Surface result(TTF_RenderUNICODE_Blended(font, reinterpret_cast<const uint16*>(text.empty()?u" ":text.c_str()), Surface::ColorSDL(textcolor)));
-        Error::IfZero(result.surface);
-        return result;
+		return (Surface&&)Surface(Error::IfZero(TTF_RenderUNICODE_Blended(font, reinterpret_cast<const uint16*>(text.empty()?u" ":text.c_str()), Surface::ColorSDL(textcolor))));
 	}
 	Surface Render(char16_t character, Color textcolor)
 	{
 		ErrorIfNotOpened();
-		Surface result(TTF_RenderGlyph_Blended(font, character, Surface::ColorSDL(textcolor)));
-        Error::IfZero(result.surface);
-        return result;
+		return (Surface&&)Surface(Error::IfZero(TTF_RenderGlyph_Blended(font, character, Surface::ColorSDL(textcolor))));
 	}
 	Surface Render(const std::string& text, Color textcolor, Color backgroundcolor)
 	{
 		ErrorIfNotOpened();
-		Surface result(TTF_RenderUTF8_Shaded(font, text.empty()?" ":text.c_str(), Surface::ColorSDL(textcolor), Surface::ColorSDL(backgroundcolor)));
-        Error::IfZero(result.surface);
-        return result;
+		return (Surface&&)Surface(Error::IfZero(TTF_RenderUTF8_Shaded(font, text.empty()?" ":text.c_str(), Surface::ColorSDL(textcolor), Surface::ColorSDL(backgroundcolor))));
 	}
 	Surface Render(const std::u16string& text, Color textcolor, Color backgroundcolor)
 	{
 		ErrorIfNotOpened();
-		Surface result(TTF_RenderUNICODE_Shaded(font, reinterpret_cast<const uint16*>(text.empty()?u" ":text.c_str()), Surface::ColorSDL(textcolor), Surface::ColorSDL(backgroundcolor)));
-        Error::IfZero(result.surface);
-        return result;
+		return (Surface&&)Surface(Error::IfZero(TTF_RenderUNICODE_Shaded(font, reinterpret_cast<const uint16*>(text.empty()?u" ":text.c_str()), Surface::ColorSDL(textcolor), Surface::ColorSDL(backgroundcolor))));
 	}
 	Surface Render(char16_t character, Color textcolor, Color backgroundcolor)
 	{
 		ErrorIfNotOpened();
-		Surface result(TTF_RenderGlyph_Shaded(font, character, Surface::ColorSDL(textcolor), Surface::ColorSDL(backgroundcolor)));
-        Error::IfZero(result.surface);
-        return result;
+		return (Surface&&)Surface(Error::IfZero(TTF_RenderGlyph_Shaded(font, character, Surface::ColorSDL(textcolor), Surface::ColorSDL(backgroundcolor))));
 	}
 	Surface RenderFast(const std::string& text, Color textcolor)
 	{
 		ErrorIfNotOpened();
-		Surface result(TTF_RenderUTF8_Solid(font, text.empty()?" ":text.c_str(), Surface::ColorSDL(textcolor)));
-        Error::IfZero(result.surface);
-        return result;
+		return (Surface&&)Surface(Error::IfZero(TTF_RenderUTF8_Solid(font, text.empty()?" ":text.c_str(), Surface::ColorSDL(textcolor))));
 	}
 	Surface RenderFast(const std::u16string& text, Color textcolor)
 	{
 		ErrorIfNotOpened();
-		Surface result(TTF_RenderUNICODE_Solid(font, reinterpret_cast<const uint16*>(text.empty()?u" ":text.c_str()), Surface::ColorSDL(textcolor)));
-        Error::IfZero(result.surface);
-        return result;
+		return (Surface&&)Surface(Error::IfZero(TTF_RenderUNICODE_Solid(font, reinterpret_cast<const uint16*>(text.empty()?u" ":text.c_str()), Surface::ColorSDL(textcolor))));
 	}
 	Surface RenderFast(char16_t character, Color textcolor)
 	{
 		ErrorIfNotOpened();
-		Surface result(TTF_RenderGlyph_Solid(font, character, Surface::ColorSDL(textcolor)));
-        Error::IfZero(result.surface);
-        return result;
+		return (Surface&&)Surface(Error::IfZero(TTF_RenderGlyph_Solid(font, character, Surface::ColorSDL(textcolor))));
 	}
 	Style GetStyle()
 	{
 		ErrorIfNotOpened();
-        return Style(TTF_GetFontStyle(font));
+		return Style(TTF_GetFontStyle(font));
 	}
 	void SetStyle(Style fontstyle)
 	{
@@ -181,7 +163,7 @@ public:
 	uint32 GetMaxHeight()
 	{
 		ErrorIfNotOpened();
-        return TTF_FontHeight(font);
+		return TTF_FontHeight(font);
 	}
 	uint32 GetAscent()
 	{
@@ -193,112 +175,112 @@ public:
 		ErrorIfNotOpened();
 		return TTF_FontDescent(font);
 	}
-    uint32 GetLineSkip()
-    {
+	uint32 GetLineSkip()
+	{
 		ErrorIfNotOpened();
-        return TTF_FontLineSkip(font);
-    }
-    uint32 GetNumFaces()
-    {
-		ErrorIfNotOpened();
-    	return TTF_FontFaces(font);
-    }
-    bool HasFixedWidth()
-    {
-		ErrorIfNotOpened();
-    	return TTF_FontFaceIsFixedWidth(font);
-    }
-    bool IsFamilyNameAvailable()
-    {
-		ErrorIfNotOpened();
-    	return TTF_FontFaceFamilyName(font);
-    }
-    std::string GetFamilyName()
-    {
-		ErrorIfNotOpened();
-    	const char* x=TTF_FontFaceFamilyName(font);
-    	return x?x:"";
+		return TTF_FontLineSkip(font);
 	}
-    bool IsStyleNameAvailable()
-    {
+	uint32 GetNumFaces()
+	{
 		ErrorIfNotOpened();
-    	return TTF_FontFaceStyleName(font);
-    }
-    std::string GetStyleName()
-    {
+		return TTF_FontFaces(font);
+	}
+	bool HasFixedWidth()
+	{
 		ErrorIfNotOpened();
-    	const char* x=TTF_FontFaceStyleName(font);
-    	return x?x:"";
+		return TTF_FontFaceIsFixedWidth(font);
+	}
+	bool IsFamilyNameAvailable()
+	{
+		ErrorIfNotOpened();
+		return TTF_FontFaceFamilyName(font);
+	}
+	std::string GetFamilyName()
+	{
+		ErrorIfNotOpened();
+		const char* x=TTF_FontFaceFamilyName(font);
+		return x?x:"";
+	}
+	bool IsStyleNameAvailable()
+	{
+		ErrorIfNotOpened();
+		return TTF_FontFaceStyleName(font);
+	}
+	std::string GetStyleName()
+	{
+		ErrorIfNotOpened();
+		const char* x=TTF_FontFaceStyleName(font);
+		return x?x:"";
 	}
 	uint32 GlyphIndex(char16_t character)
 	{
 		ErrorIfNotOpened();
-        return TTF_GlyphIsProvided(font, character);
+		return TTF_GlyphIsProvided(font, character);
 	}
-    uint32 GlyphMinX(char16_t character)
-    {
+	uint32 GlyphMinX(char16_t character)
+	{
 		ErrorIfNotOpened();
-        int result;
-        Error::IfNegative(TTF_GlyphMetrics(font, character, &result, nullptr, nullptr, nullptr, nullptr));
-        return result;
-    }
-    uint32 GlyphMinY(char16_t character)
-    {
+		int result;
+		Error::IfNegative(TTF_GlyphMetrics(font, character, &result, nullptr, nullptr, nullptr, nullptr));
+		return result;
+	}
+	uint32 GlyphMinY(char16_t character)
+	{
 		ErrorIfNotOpened();
-        int result;
-        Error::IfNegative(TTF_GlyphMetrics(font, character, nullptr, nullptr, &result, nullptr, nullptr));
-        return result;
-    }
-    uint32 GlyphMaxX(char16_t character)
-    {
+		int result;
+		Error::IfNegative(TTF_GlyphMetrics(font, character, nullptr, nullptr, &result, nullptr, nullptr));
+		return result;
+	}
+	uint32 GlyphMaxX(char16_t character)
+	{
 		ErrorIfNotOpened();
-        int result;
-        Error::IfNegative(TTF_GlyphMetrics(font, character, nullptr, &result, nullptr, nullptr, nullptr));
-        return result;
-    }
-    uint32 GlyphMaxY(char16_t character)
-    {
+		int result;
+		Error::IfNegative(TTF_GlyphMetrics(font, character, nullptr, &result, nullptr, nullptr, nullptr));
+		return result;
+	}
+	uint32 GlyphMaxY(char16_t character)
+	{
 		ErrorIfNotOpened();
-        int result;
-        Error::IfNegative(TTF_GlyphMetrics(font, character, nullptr, nullptr, nullptr, &result, nullptr));
-        return result;
-    }
-    uint32 GlyphAdvance(char16_t character)
-    {
+		int result;
+		Error::IfNegative(TTF_GlyphMetrics(font, character, nullptr, nullptr, nullptr, &result, nullptr));
+		return result;
+	}
+	uint32 GlyphAdvance(char16_t character)
+	{
 		ErrorIfNotOpened();
-        int result;
-        Error::IfNegative(TTF_GlyphMetrics(font, character, nullptr, nullptr, nullptr, nullptr, &result));
-        return result;
-    }
-    uint32 GlyphWidth(char16_t character)
-    {
+		int result;
+		Error::IfNegative(TTF_GlyphMetrics(font, character, nullptr, nullptr, nullptr, nullptr, &result));
+		return result;
+	}
+	uint32 GlyphWidth(char16_t character)
+	{
 		ErrorIfNotOpened();
-        return GlyphMaxX(character)-GlyphMinX(character);
-    }
-    uint32 GlyphHeight(char16_t character)
-    {
+		return GlyphMaxX(character)-GlyphMinX(character);
+	}
+	uint32 GlyphHeight(char16_t character)
+	{
 		ErrorIfNotOpened();
-        return GlyphMaxY(character)-GlyphMinY(character);
-    }
-    Point TextSize(const std::string& text)
-    {
+		return GlyphMaxY(character)-GlyphMinY(character);
+	}
+	Point TextSize(const std::string& text)
+	{
 		ErrorIfNotOpened();
-        Point result;
-        Error::IfNegative(TTF_SizeUTF8(font, text.c_str(), &result.x, &result.y));
-        return result;
-    }
-    Point TextSize(const std::u16string& text)
-    {
+		Point result;
+		Error::IfNegative(TTF_SizeUTF8(font, text.c_str(), &result.x, &result.y));
+		return result;
+	}
+	Point TextSize(const std::u16string& text)
+	{
 		ErrorIfNotOpened();
-        Point result;
-        Error::IfNegative(TTF_SizeUNICODE(font, reinterpret_cast<const uint16*>(text.c_str()), &result.x, &result.y));
-        return result;
-    }
-    Point TextSize(char16_t character)
-    {
+		Point result;
+		Error::IfNegative(TTF_SizeUNICODE(font, reinterpret_cast<const uint16*>(text.c_str()), &result.x, &result.y));
+		return result;
+	}
+	Point TextSize(char16_t character)
+	{
 		ErrorIfNotOpened();
-    	return Point(GlyphWidth(character), GlyphHeight(character));
-    }
+		return Point(GlyphWidth(character), GlyphHeight(character));
+	}
 };
 Font::Style operator|(Font::Style first, Font::Style second)noexcept
 {
